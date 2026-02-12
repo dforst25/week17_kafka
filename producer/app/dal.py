bcdb2048e7cab6caaa10540207796d58b082d617
David Forst <dforst@g.jct.ac.il>
2026-02-12 12:57:45 +0200

    finish with docker compose

--- producer/app/dal.py
+++ producer/app/dal.py
@@ -1,4 +1,4 @@
-from producer.app.mongo_connection import MongoConnector
+from mongo_connection import MongoConnector
 import os
 import time
 from kafka_publisher import produce_weapons
@@ -9,8 +9,8 @@ CNX = MongoConnector()
 
 def read_all_weapons_with_batches_and_publish():
     weapons = CNX.get_coll(MONGO_COLLECTION)
-    size = weapons.count_documents()
+    size = weapons.count_documents({})
     for i in range(0, size, 40):
-        batche = weapons.find().skip(i).limit(i + 40)
+        batche = weapons.find({}, {'_id': 0}).skip(i).limit(i + 40)
         produce_weapons(batche)
         time.sleep(0.5)
